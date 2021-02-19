89793b22e5879107b490990026ce7c3948c939d8
wineee <1758075541@qq.com>
2021-02-19 21:39:02 +0800

    Day 2

--- init.el
+++ init.el
@@ -1,3 +1,93 @@
+(when (>= emacs-major-version 24)
+     (require 'package)
+     (package-initialize)
+     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
+
+(require 'cl)
+;; Add Packages
+(defvar my/packages '(
+	       ;; --- Auto-completion ---
+	       company
+	       ;; --- Better Editor ---
+	       hungry-delete
+	       swiper
+	       counsel
+	       smartparens
+	       ;; --- Major Mode ---
+	       js2-mode
+	       ;; --- Minor Mode ---
+	       nodejs-repl
+	       exec-path-from-shell
+	       ;; --- Themes ---
+	       monokai-theme
+	       ;; solarized-theme
+	       ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+    (loop for pkg in my/packages
+	  when (not (package-installed-p pkg)) do (return nil)
+	  finally (return t)))
+
+(unless (my/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg my/packages)
+      (when (not (package-installed-p pkg))
+	(package-install pkg))))
+
+;; Find Executable Path on OS X
+;;(when (memq window-system '(mac ns))
+;; ((exec-path-from-shell-initialize))
+
+;; 我们可以用下面代码将 Emacs 设置为开启默认全屏，
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; 启用自动括号匹配
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; 删除连续多个空格
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+(setq hungry-delete-join-reluctantly t)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+;; 扩号
+(require 'smartparens-config)
+(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+
+;; 高亮当前行
+(global-hl-line-mode 1)
+
+;; 安装主题
+(add-to-list 'my/packages 'monokai-theme)
+(load-theme 'monokai 1)
+
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 (tool-bar-mode -1)
 
@@ -7,13 +97,13 @@
 ;; 显示行号
 (global-linum-mode 1)
 
-;; 更改光标的样式（不能生效，解决方案见第二集）
-(setq cursor-type 'bar)
+;; 更改光标的样式
+(setq-default cursor-type 'bar)
 
 ;; 关闭启动帮助画面
 (setq inhibit-splash-screen 1)
 
-;; 关闭缩进 (第二天中被去除)
+;; 关闭自动缩进
 ;; (electric-indent-mode -1)
 
 ;; 更改显示字体大小 16pt
@@ -33,6 +123,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.1)
+ '(company-minimum-prefix-length 2)
+ '(custom-safe-themes
+   '("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" default))
  '(package-selected-packages '(company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -41,5 +135,43 @@
  ;; If there is more than one, they won't work right.
  )
 
-; 开启全局 Company 补全
+;; 开启全局 Company 补全
 (global-company-mode 1)
+
+;; 禁止 Emacs 自动生成备份文件
+(setq make-backup-files nil)
+
+;; 加入最近打开过文件的选项
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+(global-set-key (kbd "C-x r") 'recentf-open-files)
+
+;; 选中一段文字 之后输入一个字符会替换掉你选中部分的文字
+(delete-selection-mode 1)
+
+;; js-2 mode
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+(require 'nodejs-repl)
+(add-hook 'js-mode-hook
+          (lambda ()
+            (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
+            (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
+            (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+            (define-key js-mode-map (kbd "C-c C-c") 'nodejs-repl-send-buffer)
+            (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+            (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+
+;; 添加 Org-mode 文本内语法高亮
+(require 'org)
+(setq org-src-fontify-natively t)
+
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/org"))
+
+;; 设置 org-agenda 打开快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
