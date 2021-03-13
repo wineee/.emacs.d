8ad2e2f36f1734a521dee4c5185a7623ddc381f5
rew <1758075541@qq.com>
2021-03-13 21:40:51 +0800

    set eaf success

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -3,7 +3,7 @@
 
 (global-set-key (kbd "C-x r") 'recentf-open-files)
 
-(global-set-key (kbd "C-w") 'backward-kill-word)
+;; (global-set-key (kbd "C-w") 'backward-kill-word)
 
 (global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "M-s e") 'iedit-mode)
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -20,12 +20,7 @@
 	       iedit
 	       helm-ag
 	       auto-yasnippet
-	       ;; --- evil ---
-	       evil
-	       ;;  evil-leader
-	       ;;  window-numbering
-	       ;; evil-surround
-	       ;; evil-nerd-commenter
+
 	       which-key
 	       ;; powerline
 	       
@@ -54,7 +49,6 @@
 
 	       use-package
 	       ) "Default packages")
-
 
 (setq package-selected-packages my/packages)
 
@@ -94,13 +88,30 @@
 ;; (setq search-default-mode #'char-fold-to-regexp)
 
 
-;; (require 'quelpa)
-;; (quelpa '(eaf :fetcher github
+;;(require 'quelpa)
+;;(quelpa '(eaf :fetcher github
 ;;              :repo  "manateelazycat/emacs-application-framework"
 ;;              :files ("*")))
 
-;; (add-to-list 'load-path "~/.emacs.d/quelpa/build/eaf")
-;; (require 'eaf)
+(use-package eaf
+  :load-path "~/.emacs.d/site-lisp/emacs-application-framework" 
+  :init
+  (use-package epc :defer t :ensure t)
+  (use-package ctable :defer t :ensure t)
+  (use-package deferred :defer t :ensure t)
+  (use-package s :defer t :ensure t)
+  :custom
+  (eaf-browser-continue-where-left-off t)
+  :config
+  (eaf-setq eaf-browser-enable-adblocker "true")
+  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;;
+
+(setq eaf-proxy-type "http")
+(setq eaf-proxy-host "127.0.0.1")
+(setq eaf-proxy-port "8889")
 
 (add-hook 'js2-mode-hook 'flycheck-mode)
 
