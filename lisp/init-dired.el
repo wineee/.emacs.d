3c2c8aa229f4a63a159e4c6481b79a47cb1fc720
rew <1758075541@qq.com>
2021-03-25 13:00:22 +0800

    init eaf.el

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -2,5 +2,11 @@
 
 (ranger-override-dired-mode t)
 
+;; helm-descbinds
 (setq helm-descbinds-window-style 'same-window)
 
+;; Hidden files (e.g. dotfiles) can be shown or hidden by pressing zh
+(setq ranger-show-hidden t)
+
+
+(provide 'init-dired)
